7a8f778d0fc3b5b0b5c509d0f4ae421da999d6f6
Kaurakoch Sriunprasert <Kaurakoch.S@gmail.com>
2025-04-10 20:23:41 +0700

    Initial commit

---
+++ src/main/java/com/Rowson/eldc_api/EldcApiApplication.java
@@ -0,0 +1,13 @@
+package com.Rowson.eldc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Rowson/eldc_api/EldcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rowson.eldc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
